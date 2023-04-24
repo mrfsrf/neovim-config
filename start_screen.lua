c4952409f848dee4c4934d77b3ac0a3f9309a7ef
Luka Juras <mrfsrf@gmail.com>
2023-04-24 10:42:18 +0200

    Refactor start screen

--- start_screen.lua
+++ start_screen.lua
@@ -5,42 +5,45 @@ function M.start_screen()
   vim.api.nvim_command("normal! ggdG")
 
   -- Display the custom start screen text
-  local start_text = {
-    [[                                                                       ]],
-    [[  ██████   █████                   █████   █████  ███                  ]],
-    [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
-    [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
-    [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
-    [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
-    [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
-    [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
-    [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
-    [[                                                                       ]],
-    "Welcome to Neovim!",
-    "To open a file, use :e <filename>",
-    "To create a new file, use :enew",
-    "For help, use :help",
-    "",
-    "Happy coding!",
-  }
 
-  -- Center the cursor vertically and horizontally
-  local lines = vim.o.lines
-  local columns = vim.o.columns
-  local start_line = math.floor((lines - #start_text) / 2)
+  local start_text = [[
 
-  for idx, text in ipairs(start_text) do
-    local start_column = math.floor((columns - #text) / 2)
-    local current_line = start_line + idx - 1
-    vim.api.nvim_buf_set_lines(0, current_line, current_line, false, {text})
-    vim.api.nvim_command("normal! " .. current_line + 1 .. "G" .. start_column + 1 .. "|")
+      ██████   █████                   █████   █████  ███                  
+     ░░██████ ░░███                   ░░███   ░░███  ░░░                   
+      ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   
+      ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  
+      ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  
+      ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  
+      █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ 
+     ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  
+                                                                            
+    Welcome to Neovim!
+    To open a file, use :e <filename>
+    To create a new file, use :enew
+    For help, use :help
+
+    Happy coding!
+  ]]
+
+  local lines_table = {}
+  for line in string.gmatch(start_text, '([^\n]+)\n') do
+    table.insert(lines_table, line)
   end
 
+  for idx, text in ipairs(lines_table) do
+    local current_line = idx
+    vim.api.nvim_buf_set_lines(0, current_line, current_line, false, {text})
+    vim.api.nvim_command("normal! " .. current_line + 1 .. "G0|")
+  end
    -- Set the file type to prevent syntax highlighting
   vim.api.nvim_command("setfiletype text")
 
   -- Mark the buffer as unmodified
   vim.api.nvim_command("setlocal nomodified")
+
+  -- Add key mapping for "i" to create new file
+  vim.api.nvim_buf_set_keymap(0, "i", "<CR>", "<ESC>:enew<CR>", {noremap = true, silent = true})
+
 end -- /end .start_screen func
 
 -- Automatically call the start_screen function if no file is given
